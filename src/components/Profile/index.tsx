3a9da05728e8427efc0d1c564899fd6aca021b28
Humberto Galdino <humberto.galdino@live.com>
2025-02-04 14:41:52 -0300

    Update profile page and create Timekeeper component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -16,42 +16,42 @@ export default function Profile() {
   const t = useTranslations("Profile");
   return (
     <>
-      <div className={`${bebasNeue.className} flex items-start rounded-2xl w-full min-h-48 p-[40px] bg-slate-600 mb-0.5`}>
+      <div className={`${bebasNeue.className} flex items-start rounded-2xl w-full min-h-48 p-[40px] shadow-input bg-gradient-to-b from-black dark:to-neutral-900 mb-0.5`}>
         <div className="flex flex-row gap-4 items-start justify-center">
-          <div className="relative w-40 h-40 outline outline-4 outline-[#6b41b6] rounded-full overflow-hidden">
+          <div className="relative w-40 h-40 border-8 border-[#6b41b6] rounded-full overflow-hidden">
             <img
               src="/profile-example.jpg"
               alt="Imagem de perfil"
               className="object-cover w-full h-full"
             />
           </div>
-          <div className="flex flex-col items-start justify-center align-middle w-fit h-[50%]">
+          <div className="flex flex-col items-start justify-center w-fit h-[50%] m-auto text-white">
             <h1 className="text-4xl ">Nome do Usuário</h1>
             <h3 className="text-xl -mt-2.5 mb-2 italic">Username</h3> 
             <div className="flex flex-row">
-              <div className="flex flex-row mr-3">
-                <RiUserFollowFill className="text-2xl mr-2 mt-3 text-black hover:text-primary cursor-pointer"/>
+              <div className="flex flex-row mr-3 text-white hover:text-primary cursor-pointer">
+                <RiUserFollowFill className="text-2xl mr-2 mt-3 "/>
                 <div className="flex flex-col">
                   <span className="text-2xl -mb-3">564</span>
                   <span className="text-lg">{t("followers")}</span>
                 </div>                
               </div>
-              <div className="flex flex-row mr-5">
-                <RiUserFollowLine className="text-2xl mr-2 mt-3 text-black hover:text-primary cursor-pointer"/>
+              <div className="flex flex-row mr-5 text-white hover:text-primary cursor-pointer">
+                <RiUserFollowLine className="text-2xl mr-2 mt-3"/>
                 <div className="flex flex-col">
                   <span className="text-2xl -mb-3">564</span>
                   <span className="text-lg">{t("following")}</span>
                 </div>
               </div>
-              <div className="flex flex-row mr-3">
-                <TbDeviceTvOldFilled className="text-2xl mr-2 mt-3 text-black hover:text-primary cursor-pointer"/>
+              <div className="flex flex-row mr-3 text-white hover:text-primary cursor-pointer">
+                <TbDeviceTvOldFilled className="text-2xl mr-2 mt-3"/>
                 <div className="flex flex-col">
                   <span className="text-2xl -mb-3">50</span>
                   <span className="text-lg">{t("tvshows")}</span>
                 </div>
               </div>
-              <div className="flex flex-row">
-                <MdLocalMovies className="text-2xl mr-2 mt-3 text-black hover:text-primary cursor-pointer"/>
+              <div className="flex flex-row text-white hover:text-primary cursor-pointer">
+                <MdLocalMovies className="text-2xl mr-2 mt-3"/>
                 <div className="flex flex-col">
                   <span className="text-2xl -mb-3">80</span>
                   <span className="text-lg">{t("movies")}</span>
