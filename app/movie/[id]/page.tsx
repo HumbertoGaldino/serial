d7600715bf94304d57cc4d205a6895eeccc181b7
Francis Bernard <francisbernardcontato@gmail.com>
2025-02-18 23:06:24 -0300

    fix error in the individual movie page: correct the expected type that comes from the API;

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,17 +1,96 @@
-'use client'
+"use client";
 
 import { useState, useEffect } from "react";
 import { fetchMovie } from "@/app/actions/fetchMovie";
 
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: null;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 export default function MoviePage() {
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState<Movie>({
+    adult: false,
+    backdrop_path: "",
+    belongs_to_collection: null,
+    budget: 0,
+    genres: [],
+    homepage: "",
+    id: 0,
+    imdb_id: "",
+    origin_country: [],
+    original_language: "",
+    original_title: "",
+    overview: "",
+    popularity: 0,
+    poster_path: "",
+    production_companies: [],
+    production_countries: [],
+    release_date: "",
+    revenue: 0,
+    runtime: 0,
+    spoken_languages: [],
+    status: "",
+    tagline: "",
+    title: "",
+    video: false,
+    vote_average: 0,
+    vote_count: 0,
+  });
 
   useEffect(() => {
     const fetchMovieData = async () => {
       try {
         const data = await fetchMovie();
         console.log("Dados recebidos:", data);
-        setMovie(data?.results || []); 
+        setMovie(data);
       } catch (error) {
         console.error("Erro ao buscar o filme:", error);
       }
@@ -28,7 +107,7 @@ export default function MoviePage() {
     <main className="flex flex-col lg-max:w-[85vw] 3xl:w-[90vw] items-center justify-center flex-1 relative z-9999 bg-slate-950">
       <div>
         <h1 className="text-white text-4xl">
-          {movie.length > 0 ? movie[0]?.title : "Carregando..."}
+          {movie.title || "Carregando..."}
         </h1>
       </div>
     </main>
